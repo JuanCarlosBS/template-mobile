43c62287f8e6c7a55cbdac41ad1691e5a64d8883
juancarlosbs <juanrov_@hotmail.com>
2022-11-22 21:38:26 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+
+import { Routes } from '@routes/index';
+
+import theme from '@theme/index';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ThemeProvider theme={theme}>
+      <View style={{ flex: 1 }}>
+        <Routes />
+      </View>
+    </ThemeProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
